735964f570d25f4b5308650ad391ba5364e3f6b3
thasmini.thayaparan <thasmini.thayaparan@noseryoung.com>
2023-10-06 13:36:54 +0200

    changed project name

--- QuoteOfTheDay/QuoteOfTheDay/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QuoteOfTheDay.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
